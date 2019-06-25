581ca94d300adec2961fd9344e93ff0220cfa1ff
Jhionan <jhionan@gmail.com>
2019-06-24 23:06:44 -0300

    create labirinto xcode

---
+++ Linguagem de Programação /Labirinto/Labirinto/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Labirinto
+//
+//  Created by Jhionan on 24/06/19.
+//  Copyright © 2019 Jhionan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
