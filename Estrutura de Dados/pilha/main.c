145ebc8b5d003c7b395839fa4e205d0d9fe515c1
Jhionan <jhionan@gmail.com>
2019-11-23 22:28:59 -0300

    add print, create, ordenate, and remove
    Signed-off-by: Jhionan <jhionan@gmail.com>

--- Estrutura de Dados/pilha/main.c
+++ Estrutura de Dados/pilha/main.c
@@ -15,17 +15,39 @@ struct Paciente
     int prioridade;
     struct Paciente *proximo;
 };
+struct Paciente *cabeca;
+
+int tamanhoFila(int imprime)
+{
+    int tamanho = 0;
+    struct Paciente *interador = cabeca;
+    while (interador != NULL)
+    {
+        tamanho++;
+        interador = interador->proximo;
+    }
+    if (imprime)
+        printf("TAMANHO DA FILA: %d", tamanho);
+    return tamanho;
+}
 
 void imprime(struct Paciente *paciente, int imprimeTodos)
 {
+    int index = 0;
+    int total = tamanhoFila(0);
     struct Paciente *atual = paciente;
     while (atual != NULL)
     {
+        int posicaoAtual = total - index;
         printf("==============================================\n");
         printf("Paciente: %s\n", atual->nome);
         printf("Telefone: %s\n", atual->telefone);
-        printf("Prioridade: %s\n", atual->nome);
-        printf("==============================================\n");
+        printf("Prioridade: %d\n", atual->prioridade);
+        if (imprimeTodos)
+        {
+            printf("Ordem na fila: %d\n", posicaoAtual);
+        }
+
         if (imprimeTodos)
         {
             atual = atual->proximo;
@@ -34,7 +56,54 @@ void imprime(struct Paciente *paciente, int imprimeTodos)
         {
             atual = NULL;
         }
+        index++;
     };
+
+    if(cabeca == NULL) {
+        printf("LISTA VAZIA!!!\n");
+    }
+}
+
+void adicionaPacienteOrdenadamente(struct Paciente *novoPaciente)
+{
+    if (novoPaciente != NULL)
+    {
+        if (cabeca == NULL)
+        {
+            cabeca = novoPaciente;
+        }
+        else
+        {
+            struct Paciente *interador = cabeca;
+            struct Paciente *proximo = NULL;
+            if (cabeca->prioridade >= novoPaciente->prioridade)
+            {
+                proximo = cabeca;
+                novoPaciente->proximo = proximo;
+                cabeca = novoPaciente;
+            }
+            else
+            {
+                while (interador->proximo != NULL)
+                {
+                    if (interador->proximo->prioridade >= interador->prioridade)
+                    {
+                        break;
+                    }
+                    interador = interador->proximo;
+                }
+                proximo = interador->proximo;
+                novoPaciente->proximo = proximo;
+                printf("§§§§§§§§§§§§§§§§§§§§§§ selecionado \n");
+                imprime(interador, 0);
+                interador->proximo = novoPaciente;
+            }
+        }
+    }
+    else
+    {
+        printf("Erro ao inserir paciente\n");
+    }
 }
 
 struct Paciente *cadastrarPaciente()
@@ -44,27 +113,49 @@ struct Paciente *cadastrarPaciente()
     scanf("%s", novoPaciente->nome);
     printf("Digite o telefone do paciente\n");
     scanf("%s", novoPaciente->telefone);
-    printf("Digite o nome do paciente\n");
-    scanf("%s", novoPaciente->nome);
+    printf("Digite a prioridade do paciente\n");
+    scanf("%d", &novoPaciente->prioridade);
     printf("Cadastrado\n");
+    novoPaciente->proximo = NULL;
+    adicionaPacienteOrdenadamente(novoPaciente);
     return novoPaciente;
 }
+
 void buscarPaciente() {}
-void proximoPacienteAOperar() {}
-void tamanhoFila() {}
+void proximoPacienteAOperar()
+{
+    struct Paciente *interador = cabeca;
+    if (interador != NULL && interador->proximo != NULL && interador->proximo->proximo!=NULL)
+    while (interador->proximo->proximo != NULL)
+    {
+        interador = interador->proximo;
+    }
+    printf("PROXIMO PARA OPERAR!! \n");
+    if(interador!= cabeca)
+    {imprime(interador->proximo, 0);}
+    else {
+        imprime(interador, 0);
+    }
+
+    if (interador == cabeca && interador->proximo == NULL)
+        cabeca = NULL;
+    interador->proximo = NULL;
+}
+
 int menu()
 {
     int option = -1;
     do
     {
         printf("==============================================\n");
-        printf("MENU");
+        printf("MENU\n");
         printf("==============================================\n");
         printf("1 - Cadastrar paciente\n");
         printf("2 - Buscar paciente\n");
         printf("3 - Proximo paciente a ser operado\n");
-        printf("4 Verificar tamanho da fila\n");
+        printf("4 - Verificar tamanho da fila\n");
         printf("5 - Sair\n");
+        printf("6 - Imprime toda lista\n");
 
         printf("Digite uma opção: ");
 
@@ -83,7 +174,10 @@ int menu()
             proximoPacienteAOperar();
             break;
         case 4:
-            tamanhoFila();
+            tamanhoFila(1);
+            break;
+        case 6:
+            imprime(cabeca, 1);
             break;
         default:
             break;
